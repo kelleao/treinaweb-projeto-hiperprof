985726c433b4ee864750169b37366dc70f531f4c
Kelleao <raquel179@gmail.com>
2023-01-25 20:43:04 -0300

    Instalando e configurando Mateiral UI

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+
+    render(): JSX.Element {
+        
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    /> 
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
