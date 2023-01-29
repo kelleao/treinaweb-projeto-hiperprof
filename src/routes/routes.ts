c979c913708243b369ff3bc0fe8ca5eb90c0776f
Kelleao <raquel179@gmail.com>
2023-01-29 11:45:21 -0300

    Configurando roteamento

---
+++ src/routes/routes.ts
@@ -0,0 +1,20 @@
+import { NextRouter } from "next/router";
+
+export const Router = {
+    home: {
+        name: '/',
+        push: function (router: NextRouter) {
+            router.push({ pathname: this.name });
+        },
+        icon: "",
+    },
+
+    pesquisaProfessor: {
+        name: "/pesquisa-professor",
+        push: function (router: NextRouter, search?: string) {
+            router.push({ pathname: this.name, query: { search } });
+        },
+        icon: "",
+    },
+
+};
