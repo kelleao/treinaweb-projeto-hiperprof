3f855ba2932a808d65f9c17e93eec5455bf1b067
Kelleao <raquel179@gmail.com>
2023-01-29 10:03:07 -0300

    Aterando tsconfig e criando componente PageTitle

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import PageTitle from "@components/data-display/PageTitle";
+
+export default function Home() {
+    return (
+        <div>
+            <PageTitle title="TITULO" subtitle="Subtitlulo exemplo" />
+        </div>
+    )
+}
---
+++ src/ui/components/data-display/PageTitle/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { PageSubtitleStyled, PageTitleContainer, PageTitleStyled } from "./styles";
+
+export interface PageTiteProps {
+    title: string;
+    subtitle?: string;
+}
+
+const PageTitle:React.FC<PageTiteProps> = ({ title, subtitle }) => {
+    return (
+        <PageTitleContainer>
+            <PageTitleStyled color={"primary"}>{title}</PageTitleStyled>
+            <PageSubtitleStyled>{subtitle}</PageSubtitleStyled>
+        </PageTitleContainer>
+    );
+};
+
+export default PageTitle;
