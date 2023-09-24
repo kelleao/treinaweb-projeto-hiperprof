6f1e1c88bfd56742c5de5eff414fd832045ea74f
Kel Leao <raquel179@gmail.com>
2023-09-24 19:50:24 -0300

    Adicionando tema na aplicacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "@mui/material";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import Base from "ui/components/surfaces/Base";
+import theme from "ui/theme/light-theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Base>
+        <Component {...pageProps} />;
+      </Base>
+    </ThemeProvider>
+  );
 }
