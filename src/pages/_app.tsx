70d11eb1ccfdcb3ab527050c64c4d0a7f99d75f1
Kel Leao <raquel179@gmail.com>
2023-09-26 19:40:46 -0300

    Adicionando contexto do professor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ProfessorProvider } from "@data/contexts/ProfessorContext";
 import { ThemeProvider } from "@mui/material";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
@@ -7,9 +8,11 @@ import theme from "ui/theme/light-theme";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Base>
-        <Component {...pageProps} />;
-      </Base>
+      <ProfessorProvider>
+        <Base>
+          <Component {...pageProps} />;
+        </Base>
+      </ProfessorProvider>
     </ThemeProvider>
   );
 }
