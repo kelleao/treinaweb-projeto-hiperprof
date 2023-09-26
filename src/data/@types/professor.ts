dedc8d43893cc8102e411442abaf5131fa0a0902
Kel Leao <raquel179@gmail.com>
2023-09-26 18:32:34 -0300

    Criando interface para cadastrar professor

--- src/data/@types/professor.ts
+++ src/data/@types/professor.ts
@@ -9,3 +9,19 @@ export interface ProfessorInterface {
   created_at: string;
   updated_at: string;
 }
+
+export interface ProfessorCadastroInterface
+  extends Omit<
+    ProfessorInterface,
+    "id" | "created_at" | "updated_at" | "valor_hora"
+  > {
+  password_confirmation: string;
+  password: string;
+  valor_hora: string | number;
+}
+
+export interface ProfessorErroInterface
+  extends Omit<ProfessorCadastroInterface, "idade" | "valor_hora"> {
+  idade: string;
+  valor_hora: string;
+}
